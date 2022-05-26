57ebbb1b18f936144a2ae2edcf3ec498c3f99f3c
MaulikGarg999 <maulik.garg@esecforte.com>
2022-05-26 15:11:13 +0530

    Fourth Commit

--- Todo.php
+++ Todo.php
@@ -6,6 +6,9 @@ $err="";
 $WorkId="";
 $Description="";
 $WorkDate="";
+$search="";
+//$arr=array();
+$wl="";
 $conn=mysqli_connect("localhost","root","","todolist");
 //===================================Insertion Logic=========================================
 if(isset($_POST["sub1"])||isset($_POST["sub5"]))
@@ -98,12 +101,14 @@ $i=1;	$c=0;
 	if($_POST["noOfrows"]>0)
 	{ 
 		//echo $_POST["noOfrows"];
+		//echo $_POST["ListOfWorks"];
 	while($i<=$_POST["noOfrows"])
-	{
-		if(isset($_POST['c'.$i]))
+	{	$cha=substr($_POST["ListOfWorks"],$i-1,1);
+		//echo "-".$cha."-";
+		if(isset($_POST['c'.$cha]))
 		{	//echo $i;
 			//echo "\n".$_POST["c".$i];
-			mysqli_query($conn,"update work set Status='Completed' where WorkId=".$i);
+			mysqli_query($conn,"update work set Status='Completed' where WorkId=".$cha);
 			$c++;
 		}
 		$i++;
@@ -127,11 +132,21 @@ if(isset($_POST["sub3"]))
 $i=1;	$c=0;
 	if($_POST["noOfrows"]>0)
 	{ 
-	while($i<=$_POST["noOfrows"])
+	/*$arr1=$_POST["ListOfWorks"];
+	$j=1;
+	while($arr1[$j])
 	{
-		if(isset($_POST['c'.$i]))
+		echo $arr1[$j];
+		$j++;
+	}	*/
+	//echo $_POST["ListOfWorks"];
+	while($i<=$_POST["noOfrows"])
+	{	
+		$cha=substr($_POST["ListOfWorks"],$i-1,1);
+		//echo "-".$cha."-";
+		if(isset($_POST['c'.$cha]))
 		{
-			mysqli_query($conn,"delete from work where WorkId=".$i);
+			mysqli_query($conn,"delete from work where WorkId=".$cha);
 			$c++;
 		}
 		$i++;
@@ -157,11 +172,14 @@ $i=1; $c=0;
 if($_POST["noOfrows"]>0)
 	{ 
 	//echo $_POST["noOfrows"];
+	//echo "\n";
+	//echo $_POST["ListOfWorks"];
 	while($i<=$_POST["noOfrows"])
-	{
-		if(isset($_POST['c'.$i]))
+	{	$cha=substr($_POST["ListOfWorks"],$i-1,1);
+		//echo "-".$cha."-";
+		if(isset($_POST['c'.$cha]))
 		{	
-			$res=mysqli_query($conn,"select * from work where WorkId=".$i);
+			$res=mysqli_query($conn,"select * from work where WorkId=".$cha);
 			$row=mysqli_fetch_assoc($res);
 			$WorkId=$row["WorkId"];
 			$Description=$row["Description"];
@@ -186,6 +204,12 @@ if($_POST["noOfrows"]>0)
 
 //=============================completed======================================================
 ?>
+<div style="height:50px;">
+<form style="text-align:right" method="get" action="Todo.php">
+<input type="text" placeholder="Search By Description" name="t4" value="">
+<input type="submit" name="sub6">
+</form>
+	</div>
 <panel>
 <form method=post action=Todo.php> 	
 <table>
@@ -198,18 +222,35 @@ if($_POST["noOfrows"]>0)
 <?php
 //=====================================Display of Todo List===================================
 $i=0;
-$res=mysqli_query($conn,"select * from work");
+if(isset($_GET["sub6"]))
+{
+	$search=$_GET["t4"];
+	$res=mysqli_query($conn, "select * from work where Description like '%".$search."%'");
+}
+else
+{
+	$res=mysqli_query($conn,"select * from work");
+}
+
 if(mysqli_num_rows($res)>0)
 {	
 	while($row=mysqli_fetch_assoc($res))	
 	{ 
 	$i++;
-	echo '<tr><td><input type=checkbox name="c'.$i.'" value="'.$i.'">'.$row["WorkId"].'</td>
+	echo '<tr><td><input type=checkbox name="c'.$row["WorkId"].'" value="'.$row["WorkId"].'">'.$row["WorkId"].'</td>
 	<td>'.$row["Description"].'</td>
 	<td>'.$row["WorkDate"].'</td>
 	<td>'.$row["Status"].'</td></tr>';
+	$wl=$wl.$row["WorkId"];
+	//echo $wl;
+	//$arr[$i]=$row["WorkId"];
 	}
-
+	/*$j=1;
+	while($arr[$j])
+	{
+		echo $arr[$j];
+		$j++;
+	}	*/
 }
 //=======================================Display of the lists completed========================
 ?>
@@ -220,13 +261,14 @@ if(mysqli_num_rows($res)>0)
 <td><input type="hidden" name="noOfrows" value="<?php echo $i ?>"></td>
 </tr>
 </table>
+<input type="hidden" name="ListOfWorks" value="<?php echo $wl ?>">
 </form>
 <!-- Insertion form for the TodoList-->
 <h3>Add another work in the Todo List</h3><br>
 <form method="post" action="Todo.php">
 <input type="text" placeholder="Serial No:" name="t1" value="<?php echo $WorkId ?>"><br>
 <!--	
-<input type="text" placeholder="Work to do..." name="t2" value="<?php echo $Description?>"><br>
+<input type="text" placeholder="Work to do..." name="t2" value=""><br>
 -->
 <textarea name="t2" placeholder="Work to do..."><?php echo $Description?></textarea><br>
 <input type="text" placeholder="YYYY-MM-DD" name="t3" value="<?php echo $WorkDate ?>"><br>
